b47a4aedc906ccf897c1571b481661bd5435262d
Cauã da Costa <cauacostacrf@gmail.com>
2023-07-25 10:42:00 -0300

    loginscreen v1.3
    Fiz diversas alterações para melhor inteligibilidade do código.

---
+++ src/dbconnect.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+class Dbconnect:
+
+    #FUNÇÃO PARA CONECTAR O BD COM O CÓDIGO
+    def conectar(self):
+        self.mydb = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='123456789caua',
+            port='3306',
+            database='pythontest'
+        )
+
+    #PEGANDO O CURSOR PARA REFERENCIAR NO CODIGO
+    def get_cursor(self):
+        return self.mydb.cursor()
