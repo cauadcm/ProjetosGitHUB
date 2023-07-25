43377ec2d2ee8c7cc248d53ff8da8f422a8e4c92
Cauã da Costa <cauacostacrf@gmail.com>
2023-07-25 11:21:27 -0300

    loginscreen v.1.4
    Alterei a senha da conexão do banco de dados para vazia, alterei o nome do banco de dados e coloquei casos de erro na conexão com o banco de dados

--- src/dbconnect.py
+++ src/dbconnect.py
@@ -1,16 +1,28 @@
 import mysql.connector
-
+from mysql.connector import Error
 class Dbconnect:
 
     #FUNÇÃO PARA CONECTAR O BD COM O CÓDIGO
     def conectar(self):
-        self.mydb = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='123456789caua',
-            port='3306',
-            database='pythontest'
-        )
+        try:
+            self.mydb = mysql.connector.connect(
+                host='localhost',
+                user='root',
+                password='',
+                port='3306',
+                database='loginscreen'
+            )
+
+            self.mydb.close()
+        except Exception as erro:
+            if erro.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Erro de autenticação: Usuário ou senha incorretos.")
+            elif erro.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+                print("O Banco de dados não existe.")
+            elif erro.errno == mysql.connector.errorcode.CR_CONN_HOST_ERROR:
+                print("Erro de conexão: Host do banco de dados não encontrado.")
+            else:
+                print(f"Erro não identificado: {erro}")
 
     #PEGANDO O CURSOR PARA REFERENCIAR NO CODIGO
     def get_cursor(self):
